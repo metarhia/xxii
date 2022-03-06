356b15abea2cd2f7f08a2bb29751b675febb285d
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-03-06 16:24:19 +0200

    Files from metarhia/Example

---
+++ application/schemas/City.js
@@ -0,0 +1,6 @@
+({
+  Entity: { x: 1 },
+
+  name: { type: 'string', unique: true, length: 80 },
+  country: 'Country',
+});
---
+++ server.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const wt = require('worker_threads');
+console.log(wt);
+
+require('impress');
