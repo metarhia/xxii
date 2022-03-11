3a10f9104faa2ce27d7e78cacf26d5c80b0b8b3a
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-03-11 02:17:28 +0200

    Remove unneeded apis

--- application/api/auth.2/register.js
+++
@@ -1,9 +0,0 @@
-({
-  access: 'public',
-  method: async ({ login, password, fullName }) => {
-    const hash = await metarhia.metautil.hashPassword(password);
-    await api.auth.provider.registerUser(login, hash, fullName);
-    const token = await context.client.startSession();
-    return { status: 'success', token };
-  },
-});
